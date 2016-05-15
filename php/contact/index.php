f7e30284eb915f6179c02b5224b4cd27b83a6539
amcdaid106 <aoife.mcdaid@gmail.com>
2016-05-15 15:15:22 +0200

    clean up duplication

--- php/contact/index.php
+++ php/contact/index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Declan McDaid</title>          
-        <link href="/static/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" href="/static/css/style.css" /> 
-        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/">Declan McDaid</a>
-                </div>
-                <ul class="nav navbar-nav">
-                  <li><a href="/gigs">Gigs</a></li>
-                  <li><a href="/media">Media</a></li>
-                  <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
+<?php include '../templates/top.php'; ?>
         
 <div class="container-fluid">
     <h1>For booking information</h1>
@@ -34,5 +14,4 @@
     </div>
 </div>
 
-    </body>
-</html>
+<?php include '../templates/bottom.php'; ?>
--- php/gigs/index.php
+++ php/gigs/index.php
@@ -1,27 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Declan McDaid</title>          
-        <link href="/static/css/bootstrap.min.css" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" href="/static/css/style.css" /> 
-        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-    </head>
-    <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="/">Declan McDaid</a>
-                </div>
-                <ul class="nav navbar-nav">
-                  <li><a href="/gigs">Gigs</a></li>
-                  <li><a href="/media">Media</a></li>
-                  <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
-        </nav>
-        
 
 
+
+<?php include '../templates/top.php'; ?>
 
     <div class="container-fluid">
     
@@ -29,8 +9,7 @@
     
     </div>
 
-    </body>
-</html>
+<?php include '../templates/bottom.php'; ?>
 
 <?php
 // {% if gigs %}
