14eb57dfad9f9bb09705b3a2e4ffe2b1e34d8844
amcdaid106 <aoife.mcdaid@gmail.com>
2016-05-16 23:43:51 +0200

    organized gigs by past and future

--- gigs/index.php
+++ gigs/index.php
@@ -3,6 +3,13 @@
     include '../templates/top.php'; 
     $data = file_get_contents("gigs.json");
     $gigs = json_decode($data, true);
+    date_default_timezone_set('EST');
+    $now = date("M j, Y g:i A");
+
+    // echo '<pre>';
+    // print_r($gigs);
+    // echo '</pre>';
+
 
     function convert_date($str) {
         $date = date_create($str);
@@ -10,30 +17,50 @@
 
     }
 
+    function print_gigs($gig_array) {
+        foreach ($gig_array as $index => $gig) {
+            $gig_date = convert_date($gig["date"]);
+            echo '<div class="gig-container">
+                    <div>
+                      <span class="glyphicon glyphicon-time bullet-icon"></span><span>' . $gig_date . '</span>
+                    </div>
+                    <div>
+                      <span class="glyphicon glyphicon-map-marker bullet-icon"></span>
+                        <a href="'. $gig["location_link"] .'" target="_blank">'. $gig["location_name"] .'</a>
+                    </div>
+                  </div>';
+        } 
+    }
+
 ?>
 
 <div class="container-fluid">
 
     <?php
-        if (count($gigs) > 0) {
-            foreach ($gigs as $index => $gig) {
-                echo '<div class="gig-container">
-                        <div>
-                          <span class="glyphicon glyphicon-time bullet-icon"></span><span>' . convert_date($gig["date"]) . '</span>
-                        </div>
-                        <div>
-                          <span class="glyphicon glyphicon-map-marker bullet-icon"></span>
-                            <a href="'. $gig["location_link"] .'" target="_blank">'. $gig["location_name"] .'</a>
-                        </div>
-                      </div>';
+        $upcoming_gigs = array();
+        $past_gigs = array();
+        foreach ($gigs as $index => $gig) {
+            if ( strtotime($gig["date"]) >= strtotime($now) ) {
+                array_push($upcoming_gigs, $gig);
+            }
+            else {
+                array_push($past_gigs, $gig);
             }
         }
-        else {
-            ?>
-                <p>No gigs at this time. Please check back soon!</p>
-            <?php
-        }   
     ?>
+    <h1>Upcoming Gigs</h1>
+    <?php
+        if (count($upcoming_gigs) > 0) {
+            print_gigs($upcoming_gigs);
+        } else {
+            echo '<div>No upcoming gigs at this time. Please check back soon for new dates!</div>';
+        }
+    ?>
+    <h1>Past Gigs</h1>
+    <?php
+        print_gigs($past_gigs);
+    ?>
+
         
 </div>
 
