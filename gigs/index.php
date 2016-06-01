94dcd90bc0c16641c49fc1fbd9329914d7f94dad
Aoife McDaid <aoife.mcdaid@gmail.com>
2016-06-01 14:41:45 -0400

    gigs and home updates

--- gigs/index.php
+++ gigs/index.php
@@ -17,7 +17,7 @@
 
     }
 
-    function print_gigs($gig_array) {
+    function print_upcoming_gigs($gig_array) {
         foreach ($gig_array as $index => $gig) {
             $gig_date = convert_date($gig["date"]);
             echo '<div class="gig-container">
@@ -31,6 +31,26 @@
                     <div>
                       <span class="glyphicon glyphicon-map-marker bullet-icon"></span>
                         <a href="' . $gig["map_link"] .'" target="blank">'.$gig["town_state"].'</a>
+                    </div>
+                  </div>';
+        }
+    }
+
+
+    function print_past_gigs($gig_array) {
+        foreach ($gig_array as $index => $gig) {
+            $gig_date = convert_date($gig["date"]);
+            echo '<div class="past-gig-container">
+                    <div>
+                      <span class="glyphicon glyphicon-time bullet-icon"></span><span>' . $gig_date . '</span>
+                    </div>
+                    <div>
+                      <span class="glyphicon glyphicon-home bullet-icon"></span>
+                        <a class="past-link" href="'. $gig["location_link"] .'" target="_blank">'. $gig["location_name"] .'</a>
+                    </div>
+                    <div>
+                      <span class="glyphicon glyphicon-map-marker bullet-icon"></span>
+                        <a class="past-link" href="' . $gig["map_link"] .'" target="blank">'.$gig["town_state"].'</a>
                     </div>
                   </div>';
         }
@@ -55,14 +75,14 @@
     <h1>Upcoming Gigs</h1>
     <?php
         if (count($upcoming_gigs) > 0) {
-            print_gigs($upcoming_gigs);
+            print_upcoming_gigs($upcoming_gigs);
         } else {
             echo '<div>No upcoming gigs at this time. Please check back soon for new dates!</div>';
         }
     ?>
     <h1>Past Gigs</h1>
     <?php
-        print_gigs($past_gigs);
+        print_past_gigs($past_gigs);
     ?>
 
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'templates/top.php'; ?>
 
 <div class="home-image-container">
-    <img src="static/images/b_and_w_looking.jpg" alt="Declan playing at Analog event" class="home-image"/>
+    <img src="static/images/b_and_w_looking_cropped.jpg" alt="Declan playing at Analog event" class="home-image"/>
 </div>
 
 <?php include 'templates/bottom.php'; ?>
